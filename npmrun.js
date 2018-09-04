fd7b35f1b6acf8f4b56a1d172817b477a87cd94a
pekka <pekka.siiriainen@mirumagency.com>
2018-09-04 16:25:01 +0300

    -p

--- npmrun.js
+++ npmrun.js
@@ -3,15 +3,23 @@ const dir = process.cwd()
 const args = process.argv
 const colors = require('colors')
 
+const prompt = args[2] === '-p'
+
 const packageJsonPath = dir + '/package.json'
 if (!fs.existsSync(packageJsonPath)) {
-  return console.log('Error: No package.json found in this directory.'.bgRed.white)
+  return console.log('ERROR: No package.json found in this directory.'.bgRed.white)
 }
 const scripts = JSON.parse(fs.readFileSync(packageJsonPath).toString()).scripts
 const scriptCommands = Object.keys(scripts)
 
 console.log('\n')
 console.log('Available scripts:'.green)
-console.log(scriptCommands.join('\n').bgGreen.black)
-console.log('\n')
 
+if (!prompt) {
+  console.log(scriptCommands.join('\n').bgGreen.black)
+} else {
+  console.log('TODO: Switch...')
+  console.log(scriptCommands.join('\n').bgGreen.black)
+}
+
+console.log('\n')
