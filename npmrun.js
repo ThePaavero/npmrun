b39cf98f8854ec519223d4b48ee2c5ea5cdc0667
pekka <pekka.siiriainen@mirumagency.com>
2018-09-04 16:58:56 +0300

    Done for now.

--- npmrun.js
+++ npmrun.js
@@ -1,28 +1,24 @@
 const fs = require('fs')
-const util = require('util')
 const dir = process.cwd()
 const args = process.argv
-const colors = require('colors')
-const prompt = args[2] === '-p'
+const noPrompt = args[2] === '-list'
 const select = require('select-prompt')
 const spawn = require('child_process').spawn
-
+require('colors')
 const npm = (process.platform === "win32" ? "npm.cmd" : "npm")
 
-if (prompt) {
-}
-
 const packageJsonPath = dir + '/package.json'
+
 if (!fs.existsSync(packageJsonPath)) {
   return console.log('ERROR: No package.json found in this directory.'.bgRed.white)
 }
+
 const scripts = JSON.parse(fs.readFileSync(packageJsonPath).toString()).scripts
 const scriptCommands = Object.keys(scripts)
 
-console.log('\n')
 console.log('Available scripts:'.green)
 
-if (!prompt) {
+if (noPrompt) {
   console.log(scriptCommands.join('\n').bgGreen.black)
 } else {
   const options = scriptCommands.map(c => {
@@ -31,19 +27,17 @@ if (!prompt) {
       value: c
     }
   })
-  select('Select a script to run', options)
-    .on('submit', chosen => {
-      console.log(chosen)
-      // const command = spawn('npm run ' + chosen)
-      const command = spawn(npm, ['run', chosen])
-      command.on('data', data => {
-        console.log(data.toString())
-      })
-      command.stderr.on('data', data => {
-        console.log('stderr: ' + data.toString())
-      })
-      command.on('exit', code => {
-        console.log('child process exited with code ' + code.toString())
-      })
+  select('Select a script to run', options).on('submit', chosen => {
+    console.log('Running script "chosen"...')
+    const command = spawn(npm, ['run', chosen])
+    command.on('data', data => {
+      console.log(data.toString())
     })
+    command.stderr.on('data', data => {
+      console.log('stderr: ' + data.toString())
+    })
+    command.on('exit', code => {
+      console.log('child process exited with code ' + code.toString())
+    })
+  })
 }
