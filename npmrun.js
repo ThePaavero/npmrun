486f484922005a0fa619034dccb1c8185400d27a
pekka <pekka.siiriainen@mirumagency.com>
2018-09-04 16:47:15 +0300

    :)

--- npmrun.js
+++ npmrun.js
@@ -2,8 +2,11 @@ const fs = require('fs')
 const dir = process.cwd()
 const args = process.argv
 const colors = require('colors')
-
 const prompt = args[2] === '-p'
+const select = require('select-prompt')
+
+if (prompt) {
+}
 
 const packageJsonPath = dir + '/package.json'
 if (!fs.existsSync(packageJsonPath)) {
@@ -18,8 +21,16 @@ console.log('Available scripts:'.green)
 if (!prompt) {
   console.log(scriptCommands.join('\n').bgGreen.black)
 } else {
-  console.log('TODO: Switch...')
-  console.log(scriptCommands.join('\n').bgGreen.black)
+  const options = scriptCommands.map(c => {
+    return {
+      title: c,
+      value: c
+    }
+  })
+  select('Select a script to run', options)
+    .on('submit', chosen => {
+      console.log(chosen)
+    })
 }
 
 console.log('\n')
